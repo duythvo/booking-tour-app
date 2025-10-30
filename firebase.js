e3d49adc1a36ae7ad605d6c9a885a8bf76480c74
duythvo <duythvo2004@gmail.com>
2025-10-30 20:15:46 +0700

    a

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDOvx5t4P1O9280bm9mH4i081cU7fp-qoM",
+  authDomain: "booking-tour-project-5d086.firebaseapp.com",
+  projectId: "booking-tour-project-5d086",
+  storageBucket: "booking-tour-project-5d086.firebasestorage.app",
+  messagingSenderId: "317333256179",
+  appId: "1:317333256179:web:2a35915db969286e6fc486"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
